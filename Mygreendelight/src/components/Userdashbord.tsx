2149d2367b63db3e217eb9a4273da98424c2db4f
Anurag Singh <anuragsinghas098@gmail.com>
2026-09-07 17:03:31 +0530

    Remove redundant category section for cleaner homepage flow

--- Mygreendelight/src/components/Userdashbord.tsx
+++ Mygreendelight/src/components/Userdashbord.tsx
@@ -114,10 +114,7 @@ export default async function Userdashbord() {
       {/* 1.5 Quick Commerce Trust & Speed Ribbon */}
       <TrustRibbon />
 
-      {/* 2. Shop by Category Slider */}
-      <Categoryslider categories={plainCategories} />
-
-      {/* 3. Interactive Fresh Produce Section (3 Tabs: Vegetables, Fruits, Exotics + Grid/List Switcher) */}
+      {/* 2. Interactive Fresh Produce Section (3 Tabs: Vegetables, Fruits, Exotics + Grid/List Switcher) */}
       <FilteredProduceSection groceries={plainNew} />
 
       {/* 4. Live Flash Deals & Steal Discounts */}
